5fa7ba84d3cd4e5f52424afdacf1154322958942
oprogramador <uomodislesia@gmail.com>
2017-04-08 16:06:15 +0200

    food visualization

--- draw.js
+++ draw.js
@@ -8,6 +8,12 @@ function draw() {
       let tableCell = document.createElement('td');
       let cell = state.cells[JSON.stringify({ x, y })];
       tableCell.className = cell.obstacle ? 'cell full' : 'cell empty';
+      if (!cell.obstacle) {
+        let foodImg = document.createElement('img');
+        foodImg.src = 'img/hamburger.png';
+        foodImg.style.width = cell.food * 20;
+        tableCell.appendChild(foodImg);
+      }
       tableCell.onclick = function() {
         cell.modifyObstacle();
         draw();
