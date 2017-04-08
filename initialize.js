d5a56f5ee4f25553fda71fd8d81355173ddb3ff2
oprogramador <uomodislesia@gmail.com>
2017-04-08 15:27:31 +0200

    obstacles

--- initialize.js
+++ initialize.js
@@ -7,7 +7,17 @@ function initialize() {
     var row = document.createElement('tr');
     for (let x = 0; x < width; x++) {
       var cell = document.createElement('td');
-      cell.className = 'cell';
+      cell.className = 'cell empty';
+      state.cells[JSON.stringify({ x, y })] = {
+        food: Math.random(),
+        obstacle: false,
+      }
+      cell.onclick = function() {
+        var oldObstacle = state.cells[JSON.stringify({ x, y })].obstacle;
+        var newObstacle = !oldObstacle;
+        state.cells[JSON.stringify({ x, y })].obstacle = newObstacle;
+        this.className = newObstacle ? 'cell full' : 'cell empty';
+      }
       row.appendChild(cell);
     }
     board.appendChild(row);
